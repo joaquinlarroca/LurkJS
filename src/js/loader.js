d3691e625dde60a3eb9bb13ff98636a0c76f0e5a
Joaquin Larroca <52870198+joaquinlarroca@users.noreply.github.com>
2024-07-13 01:03:25 -0300

    130720240103

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,10 +1,15 @@
 import { global, screen } from "./main.js"
+const loadingBarColor = screen.css.computedStyles.getPropertyValue('--loading-bar-done-color').trim();
+let loadingBackgroundColor = screen.css.computedStyles.getPropertyValue('--loading-screen-background').trim();
+loadingBackgroundColor = "#000000"
+screen.loading.logo_bg = loadingBackgroundColor
 let load = setInterval(() => {
-    screen.loading.assets_loaded_bar.style.width = global._assets_to_load_done / global._assets_to_load_count * 100 + "%"
+    var percent = global._assets_to_load_done / global._assets_to_load_count * 100
+    screen.loading.bar.style.background = `linear-gradient(90deg, ${loadingBarColor} ${percent}%, ${loadingBackgroundColor} ${percent}%)`
     if (global._assets_to_load_count == global._assets_to_load_done) {
         global._assets_have_loaded = true
     }
-    if (global._assets_have_loaded) {
+    if (global._assets_have_loaded && false) {
         clearInterval(load)
         setTimeout(() => {
             screen.loading.background.style.opacity = "0"
